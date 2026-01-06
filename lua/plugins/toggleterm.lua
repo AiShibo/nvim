f4566ee5852d162d51c6acda19ae57a4a0917000
Shibo Ai <shibo@if.local>
2026-01-05 22:08:13 -0800

    Fix toggleterm keymaps

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,10 +4,26 @@ return {
   config = function()
     require("toggleterm").setup({
       direction = "float",
-      open_mapping = [[<leader>tt]],
-      start_in_insert = true,
+      open_mapping = nil,
+      start_in_insert = false,
       shade_terminals = false,
+    })
+
+    vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", {
+      silent = true,
+      desc = "Toggle terminal",
+    })
+
+    vim.api.nvim_create_autocmd("TermOpen", {
+      group = vim.api.nvim_create_augroup("ToggletermKeymaps", { clear = true }),
+      pattern = "term://*toggleterm#*",
+      callback = function(event)
+        vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], {
+          buffer = event.buf,
+          silent = true,
+          desc = "Exit terminal mode",
+        })
+      end,
     })
   end,
 }
-
