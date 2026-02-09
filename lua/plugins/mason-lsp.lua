4f74fa31a75a2418b6303b119f83637501bb7c2e
Shibo Ai <asnicoria@gmail.com>
2026-02-09 14:24:15 -0800

    fix the leader+e issue

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -39,7 +39,7 @@ return {
         -- Diagnostics
         map("n", "[d", vim.diagnostic.goto_prev, "Prev diagnostic")
         map("n", "]d", vim.diagnostic.goto_next, "Next diagnostic")
-        map("n", "<leader>e", vim.diagnostic.open_float, "Line diagnostics")
+        map("n", "<leader>de", vim.diagnostic.open_float, "Line diagnostics")
         map("n", "<leader>q", vim.diagnostic.setloclist, "Diagnostics list")
 
         -- Formatting
